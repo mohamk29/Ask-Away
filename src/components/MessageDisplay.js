0d06f949b7c4fa371e2dca3f9dc9c1f6ebb0580c
mohamk29 <mohamk29@my.yorku.ca>
2023-10-29 18:44:03 -0400

    Added Auto-Scroll Behavior

--- src/components/MessageDisplay.js
+++ src/components/MessageDisplay.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styles from "./MessageDisplay.module.css";
 
 const MessageDisplay = ({ messages }) => {
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className={styles.messageDisplay}>
       {messages.map((msg, index) => (
@@ -9,6 +15,7 @@ const MessageDisplay = ({ messages }) => {
           {msg.text}
         </div>
       ))}
+      <div ref={messagesEndRef}></div>
     </div>
   );
 };
