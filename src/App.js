2a5b969f6328318da86503e0e52a29af1d75c3c6
mohamk29 <mohamk29@my.yorku.ca>
2023-10-30 17:22:01 -0400

    Added dark themed style to the application

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+
       <MessageDisplay messages={messages} />
       {loading && <p className="typingIndicator">Typing...</p>}
       <ChatBox onSendMessage={handleSendMessage} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return <header className={styles.header}>Ask Away</header>;
+  return (
+    <header className={styles.header}>
+      <span className={styles.ask}>Ask</span>
+      <span className={styles.away}>Away</span>
+    </header>
+  );
 };
 
 export default Header;
